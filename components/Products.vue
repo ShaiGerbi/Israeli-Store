c1ab7b97e56556c35de1c77c3860b3d5beb7be93
Shai Gerbi <ShaiGerbi@gmail.com>
2020-05-20 21:21:39 +0300

    Create Products component

---
+++ components/Products.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-row>
+    <v-col v-for="item in items" :key="item.id" xs="12" sm="6" md="4">
+      <v-card>
+
+        <v-img :src="item.src" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
+
+        <v-card-actions>
+
+          <v-btn color="primary" href="mailto:ShaiGerbiApps@gmail.com" text>
+            Buy It Now
+          </v-btn>
+
+          <v-spacer />
+
+          <v-btn icon>
+            <v-icon>mdi-share-variant</v-icon>
+          </v-btn>
+
+        </v-card-actions>
+
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+
+  name: 'Products',
+
+  props: {
+    items: {
+      require: true,
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+
+}
+</script>
