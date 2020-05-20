361f6d0e05e13f86091a0617f399259fd77667d6
Shai Gerbi <ShaiGerbi@gmail.com>
2020-05-20 17:20:48 +0300

    Create TheHeader component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,7 @@
 <template>
   <v-app dark>
 
-    <v-navigation-drawer v-model="drawer" fixed app>
-      <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
-    </v-app-bar>
+    <the-header />
 
     <v-content>
       <v-container>
@@ -33,19 +17,13 @@
 </template>
 
 <script>
+import TheHeader from '@/components/TheHeader'
+
 export default {
-  data () {
-    return {
-      drawer: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Home',
-          to: '/'
-        }
-      ],
-      title: 'Vuetify.js'
-    }
+
+  components: {
+    TheHeader
   }
+
 }
 </script>
