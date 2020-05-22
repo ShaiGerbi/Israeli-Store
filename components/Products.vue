b67f7fb9e17c2360ea70e6ec5622b09a86120b58
Shai Gerbi <ShaiGerbi@gmail.com>
2020-05-22 16:32:18 +0300

    Add image loader

--- components/Products.vue
+++ components/Products.vue
@@ -3,7 +3,13 @@
     <v-col v-for="item in items" :key="item.id" xs="12" sm="6" md="4">
       <v-card>
 
-        <v-img :src="item.src" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
+        <v-img :src="item.src" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)">
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular size="50" color="blue lighten-4" indeterminate></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
 
         <v-card-actions>
 
